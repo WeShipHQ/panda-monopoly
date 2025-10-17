e67e51771312501f4658c1159bd678b08f56c460
kienhoang3397 <125807253+kienhoang3397@users.noreply.github.com>
2025-10-18 01:01:53 +0700

    feat: Update sound assets and add new sound effects for player actions and game events

--- web/lib/soundUtil.ts
+++ web/lib/soundUtil.ts
@@ -44,7 +44,7 @@ const soundMap = {
   // UI interaction sounds
   "button-click": "/sounds/button-click.mp3",
   "button-hover": "/sounds/button-hover.mp3",
-  "button-click2": "/sounds/button-click2.mp3",
+  "button-click2": "/sounds/button-click.mp3",
 
   // Game event sounds
   "property-buy": "/sounds/property-buy.mp3",
@@ -53,12 +53,13 @@ const soundMap = {
   "rent-pay": "/sounds/money-pay.mp3",
   "house-build": "/sounds/up-house.wav",
   // "house-build": "/sounds/property-buy.mp3",
-  "hotel-build": "/sounds/property-buy.mp3",
+  // "hotel-build": "/sounds/property-buy.mp3",
+  "hotel-build": "/sounds/up-house.wav",
 
   // Special events
   jail: "/sounds/jail.mp3",
   win: "/sounds/win.mp3",
-  lose: "/sounds/lose.mp3",
+  lose: "/sounds/lose.wav",
 
   // Fun sounds
   "anime-wow": "/sounds/anime-wow.mp3",
@@ -66,8 +67,8 @@ const soundMap = {
   "vine-boom": "/sounds/vine-boom.mp3",
 
   // Game events
-  "player-join": "/sounds/button-click.mp3",
-  "player-leave": "/sounds/button-click2.mp3",
+  "player-join": "/sounds/user-join.mp3",
+  "player-leave": "/sounds/user-off.mp3",
   "game-start": "/sounds/game-start.mp3",
   "game-cancel": "/sounds/bruh.mp3",
   "building-sell": "/sounds/money-receive.mp3",
